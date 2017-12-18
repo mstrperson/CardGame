f35a1df51ab10b70417efeb1f388c6a6aa428cf1
Jason Cox <coxj@vt.edu>
2017-12-18 10:51:26 -0700

    comments!

--- CardGame/Card.cs
+++ CardGame/Card.cs
@@ -7,6 +7,10 @@ namespace CardGame
     public class Card : IComparable<Card>
     {
         #region Helper Classes
+        /// <summary>
+        /// Represent the various suits of a deck of cards.
+        /// Suits are not comparable other than Equality.
+        /// </summary>
         public class Suit
         {
             public static Suit Spades = new Suit("Spades");
@@ -34,10 +38,15 @@ namespace CardGame
             }
         }
 
+        /// <summary>
+        /// Represents the Values associated with cards.  These have numeric values as well as textual descriptions.
+        /// Values are comparable and implement boolean comparison operators.
+        /// for use in Scoring, Values also implement arithmetic operators + and - and return integers.
+        /// </summary>
         public class Value
         {
-            public int value;
-            public String title;
+            public readonly int value;
+            public readonly String title;
 
             private Value(int value, string title)
             {
@@ -53,6 +62,14 @@ namespace CardGame
 
             public static bool operator >=(Value a, Value b) => a.value >= b.value;
             public static bool operator <=(Value a, Value b) => a.value <= b.value;
+
+            public static int operator -(Value a, Value b) => a.value - b.value;
+            public static int operator -(int a, Value b) => a - b.value;
+            public static int operator -(Value a, int b) => a.value - b;
+
+            public static int operator +(Value a, Value b) => a.value + b.value;
+            public static int operator +(int a, Value b) => a + b.value;
+            public static int operator +(Value a, int b) => a.value + b;
 
             public override string ToString() => this.title;
 
@@ -71,9 +88,15 @@ namespace CardGame
             public static Value King = new Value(13, "King");
             public static Value AceHigh = new Value(14, "Ace");
 
+            /// <summary>
+            /// Statically defined Values in useful List format ordered from AceLow to King
+            /// </summary>
             public static List<Value> AcesLowValues = new List<Value>()
             { AceLow, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
 
+            /// <summary>
+            /// Statically defined Values in useful List format ordered from 2 to AceHigh.
+            /// </summary>
             public static List<Value> AcesHighValues = new List<Value>()
             { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, AceHigh };
         }
@@ -92,7 +115,7 @@ namespace CardGame
 
         int IComparable<Card>.CompareTo(Card obj)
         {
-            return this.MyValue.value - ((Card)obj).MyValue.value;
+            return this.MyValue - ((Card)obj).MyValue;
         }
     }
 }
